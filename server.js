96c17365f38793d51cf6a0ee57b4b2ee5942a0a9
NelsonPetitos <nde_nelson@yahoo.fr>
2016-10-17 13:21:23 +0100

    v7

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/initpopup', function(req, res){
             res.end();
         }
         else {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(content, 'utf-8');
         }
     });
