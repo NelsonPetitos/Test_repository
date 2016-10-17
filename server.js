2bb832fad8eb6dfee56a1f20ea8547d067edf39d
NelsonPetitos <nde_nelson@yahoo.fr>
2016-10-17 18:34:04 +0100

    V8

--- server.js
+++ server.js
@@ -34,9 +34,11 @@ var i = 0;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
+    console.log("Nouvelle connection à l'accueil du serveur.");
 });
 
 app.get('/initpopup', function(req, res){
+    console.log("Nouvelle connection à l'url /initpopup");
     fs.readFile(__dirname + '/public/popup.html', function(error, content) {
         if (error) {
             res.writeHead(500, { 'Content-Type': 'text/html' });
@@ -56,13 +58,16 @@ io.on('connection', function(socket){
     i = i + 1;
 
     socket.on('message', function(message){
+        console.log("Le sereur viens recoit une demande de "+ socket.id);
         message.userid = socket.id
         io.emit('mobile', message);
+        console.log("Le sereur a transmis le message au modem.");
     });
 
     socket.on('mobile', function (result) {
         var j;
         var obj = JSON.parse(result);
+        console.log("Le module viens de repondre au serveur.");
         // console.log(listSocket.length);
         // console.log(listSocket[0].id);
         // console.log(obj.somme);
@@ -73,11 +78,12 @@ io.on('connection', function(socket){
         }
         if( j <= i ){
             console.log(result);
+            console.log("Le sereur répond a la socket "+ listSocket[j].id);
             listSocket[j].emit('result', obj);
         }
     });
 });
 
 http.listen((process.env.PORT || 5000), function(){
-    console.log('listening on port : '+ (process.env.PORT || 5000));
+    console.log('Le serveur écoute sur le port : '+ (process.env.PORT || 5000));
 });
