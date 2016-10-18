f2cbae80382945076ec2f20afdfbb1e5154a51a7
NelsonPetitos <nde_nelson@yahoo.fr>
2016-10-18 09:49:06 +0100

    first one not working

--- server.js
+++ server.js
@@ -24,7 +24,6 @@
 // console.log("App listening on port 8081");
 
 var app = require('express')();
-var cors = require('cors')
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -33,7 +32,25 @@ var fs = require('fs');
 var listSocket = new Array();
 var i = 0;
 
-app.use(cors())
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
